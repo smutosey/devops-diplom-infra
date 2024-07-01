fd3b826a4945846571de4007954e1ed76675c65a
alexander.nedorezov <alserned@gmail.com>
2024-07-02 01:36:48 +0700

    fix cloud init

--- production/ansible.tf
+++ production/ansible.tf
@@ -20,3 +20,41 @@ resource "local_file" "ansible_inventory" {
   )
   filename = "./ansible/inventory.yml"
 }
+#
+# resource "null_resource" "kill_apt_controllers" {
+#   depends_on = [
+#     yandex_compute_instance_group.worker,
+#     yandex_compute_instance_group.control-plane,
+#   ]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo killall apt apt-get unattended-upgr",
+#     ]
+#     connection {
+#       type     = "ssh"
+#       user     = var.admin
+#       private_key = file(var.ssh_private_key)
+#       host = yandex_compute_instance_group.control-plane.instances.2.network_interface.0.ip_address
+#       bastion_host = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
+#     }
+#   }
+# }
+#
+# resource "null_resource" "kill_apt_workers" {
+#   depends_on = [
+#     yandex_compute_instance_group.worker,
+#     yandex_compute_instance_group.control-plane,
+#   ]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo killall apt apt-get unattended-upgr",
+#     ]
+#     connection {
+#       type     = "ssh"
+#       user     = var.admin
+#       private_key = file(var.ssh_private_key)
+#       host = yandex_compute_instance_group.worker.instances.2.network_interface.0.ip_address
+#       bastion_host = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
+#     }
+#   }
+# }
