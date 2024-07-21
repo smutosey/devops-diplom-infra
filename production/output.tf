0a2a3b9decff22bc3ca792433fcfcc471c865969
alexander.nedorezov <alserned@gmail.com>
2024-07-21 17:46:17 +0700

    workflow: terraform + kubespray

--- production/output.tf
+++ production/output.tf
@@ -1,7 +1,7 @@
-output "bastion_nat_ip" {
-  value = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
-}
-
-output "lb_nat_ip" {
-  value = yandex_vpc_address.lb_addr.external_ipv4_address[0].address
-}
+# output "bastion_nat_ip" {
+#   value = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
+# }
+#
+# output "lb_nat_ip" {
+#   value = yandex_vpc_address.lb_addr.external_ipv4_address[0].address
+# }
